dc6ec0e4abfefdc37b70d3837e1b73fa94470d31
atha <athabarokahfc@gmail.com>
2024-03-19 10:38:40 +0700

    fix: update order handler problem cause deleting order

--- Assignment_2/handler/handler.go
+++ Assignment_2/handler/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"orders_management/database"
 	"orders_management/model"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -61,29 +62,50 @@ func CreateOrder(c *gin.Context) {
 
 
 func UpdateOrder(c *gin.Context) {
-	orderID := c.Param("orderId")
-	var updatedOrder model.Order
-	if err := c.BindJSON(&updatedOrder); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Format JSON tidak valid"})
-		return
-	}
+    orderID := c.Param("orderId") // Mengambil orderId dari path
+    
+    var updatedOrder model.Order
+    if err := c.BindJSON(&updatedOrder); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Format JSON tidak valid"})
+        return
+    }
 
-	db, err := database.Connect()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal terhubung ke database"})
-		return
-	}
-	defer database.Close(db)
+    orderIDInt, _ := strconv.Atoi(orderID)
+   
+    updatedOrder.OrderID = uint(orderIDInt)
 
+    db, err := database.Connect()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal terhubung ke database"})
+        return
+    }
+    defer database.Close(db)
 
-	// Perbarui pesanan yang ada di database
-	if err := db.Where("order_id = ?", orderID).Updates(&updatedOrder).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal memperbarui pesanan"})
-		return
-	}
+    // Konversi format tanggal
+    orderedAt, err := time.Parse("2006-01-02T15:04:05-07:00", updatedOrder.OrderedAt)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengonversi format tanggal"})
+        return
+    }
+    updatedOrder.OrderedAt = orderedAt.Format("2006-01-02 15:04:05")
 
-	c.JSON(http.StatusOK, gin.H{"message": "Pesanan berhasil diperbarui", "data": updatedOrder})
+    // Perbarui pesanan yang ada di database berdasarkan orderId
+    if err := db.Where("order_id = ?", orderID).Updates(&updatedOrder).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal memperbarui pesanan"})
+        return
+    }
+
+    // Update item sesuai dengan ItemId
+    for i := range updatedOrder.Items {
+        if err := db.Model(&model.Item{}).Where("item_id = ?", updatedOrder.Items[i].ItemID).Updates(&updatedOrder.Items[i]).Error; err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal memperbarui item"})
+            return
+        }
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "Pesanan berhasil diperbarui", "data": updatedOrder})
 }
+
 
 
 func DeleteOrder(c *gin.Context) {
