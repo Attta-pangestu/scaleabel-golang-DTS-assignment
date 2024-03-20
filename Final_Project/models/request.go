320941cf343ef3f8db1e5ea18fdd6d28acd36596
atha <athabarokahfc@gmail.com>
2024-03-20 11:38:40 +0700

    create: user, photo, comment model

---
+++ Final_Project/models/request.go
@@ -0,0 +1,29 @@
+package models
+
+// RequestUserLogin represents the model for an User Login
+type RequestUserLogin struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RequestUserRegister struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	Age      uint   `json:"age"`
+}
+
+type RequestSocialMedia struct {
+	Name           string `json:"name"`
+	SocialMediaUrl string `json:"social_media_url"`
+}
+
+type RequestPhoto struct {
+	Title    string `json:"title"`
+	Caption  string `json:"caption"`
+	PhotoUrl string `json:"photo_url"`
+}
+
+type RequestComment struct {
+	Comment string `json:"comment_message"`
+}
